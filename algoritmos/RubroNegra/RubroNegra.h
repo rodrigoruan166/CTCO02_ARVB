c330343dfe695f7d8cc28a73fdb3e4633497c3f4
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-06-27 01:37:34 -0300

    Atualiza .h com assinaturas da rubro negro

--- algoritmos/RubroNegra/RubroNegra.h
+++ algoritmos/RubroNegra/RubroNegra.h
@@ -14,3 +14,27 @@
 typedef struct NO noRB;
 
 typedef struct RB rb;
+
+rb *alocaArvore();
+
+noRB *alocaNo(rb *arv, int chave);
+
+void insereNo(rb *T, noRB *z);
+
+void RB_Insert_Fixup(rb *T, noRB *z);
+
+void RB_Transplant(rb *T, noRB *u, noRB *v);
+
+noRB *Tree_Minimum(rb *T, noRB *x);
+
+void left_rotate(rb *T, noRB *y);
+
+void right_rotate(rb *T, noRB *x);
+
+void percorrePreOrdem(rb *T, noRB *aux);
+
+noRB *retornaRaiz(rb *T);
+
+int removeNo(rb *T, int chave);
+
+void RB_Delete_Fixup(rb *T, noRB *x);
