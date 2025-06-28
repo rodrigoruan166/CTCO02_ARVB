2ad5829b761b05f9db6773a460746d43a1e097f4
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-06-28 14:21:37 -0300

    Adiciona teste 1 para arvore 234

---
+++ testes/teste234/teste234.c
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * Arquivo: teste234.c
+ *
+ * Descrição: Testes unitários para a árvore 2-3-4 implementada
+ *            em /algoritmos. Este arquivo contém os casos de teste para
+ *            verificar a corretude e robustez da árvore 2-3-4.
+ *
+ * Criadores:
+ *            - CAIO MENDES COUTINHO E GARCIA - 2024007305
+ *            - DIEGO SILVA CORTEZ - 2024002480
+ *            - RODRIGO RUAN SOUZA VIANA - 2024004644
+ *
+ * Data: 28/06/2025
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../algoritmos/Arvore234/Arvore234.h"
+#include "./teste234.h"
+
+const int NUMBER_OF_TEST_CASES = 1;
+f functions[] = {
+    teste1};
+
+void teste1()
+{
+    arv234 *arv = alocaArvore();
+
+    if (!arv)
+        return;
+
+    int chaves[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    for (int i = 0; i < sizeof(chaves) / sizeof(int); i++)
+    {
+        insereChave(chaves[i], arv);
+    }
+
+    imprimirPorNivel(arv);
+}
+
+int main()
+{
+    for (int i = 0; i < NUMBER_OF_TEST_CASES; i++)
+    {
+        printf(" ------ CASO %d ------\n", i + 1);
+        functions[i]();
+    }
+
+    return 0;
+}
