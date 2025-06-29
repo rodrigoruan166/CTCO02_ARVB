f7e130e5f5fd1c22dfd370a0c98640961c1fee47
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-06-29 16:31:05 -0300

    Adiciona testes para conversor

---
+++ testes/testeConversor/testeConversor.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Arquivo: testeConversor.c
+ *
+ * Descrição: Testes unitários para a conversão implementada
+ *            em /algoritmos. Este arquivo contém os casos de teste para
+ *            verificar a corretude e robustez da conversão.
+ *
+ * Criadores:
+ *            - CAIO MENDES COUTINHO E GARCIA - 2024007305
+ *            - DIEGO SILVA CORTEZ - 2024002480
+ *            - RODRIGO RUAN SOUZA VIANA - 2024004644
+ *
+ * Data: 29/06/2025
+ ******************************************************************************/
+typedef void (*f)();
+
+void teste1();
+
+void teste2();
+
+void teste3();
+
+void teste4();
+
+void teste5();
+
+void teste6();
+
+void teste7();
+
+void teste8();
+
+void teste9();
+
+void teste10();
+
+void teste11();
+
+void teste12();
